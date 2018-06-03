7b1d330b01ee6340d6b6fc72dbf64830173e6a03
mldn <litiantain45@163.com>
2018-06-03 10:30:29 +0800

    change Hello.java In dev

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ public class Hello{
 		System.out.println("Hello java");
 		System.out.println("Yoooooo");
 		System.out.println("Hahahahaha");
-		System.out.println("How are you");
+		// System.out.println("How are you");
+		System.out.println("就是这样吧");	
 	}
 }
